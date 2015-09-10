300d3e8dec642e543ef88dcb6a1fd2518c69a937
dsun092 <dsun092@gmail.com>
2015-09-09 23:08:49 -0700

    marker on map

--- js/kazem.js
+++ js/kazem.js
@@ -7,3 +7,34 @@ $(window).bind('scroll', function () {
         $('.main_menu').removeClass('fixed');
     }
 });
+
+var map = new google.maps.Map(document.getElementById('map1'), {
+    center: {lat: 34.014440, lng: -118.407727},
+    scrollwheel: false,
+    zoom: 13,
+    scaleControl: false,
+    draggable: false,
+    navigationControl: false,
+     scrollwheel: false,
+  });
+
+  var map2 = new google.maps.Map(document.getElementById('map2'), {
+    center: {lat: 33.665279, lng: -117.763177},
+    scrollwheel: false,
+    zoom: 13,
+    scaleControl: false,
+    draggable: false,
+    navigationControl: false,
+     scrollwheel: false,
+  });
+ console.log("test");
+
+var marker1 = new google.maps.Marker({
+    position: {lat: 34.014440, lng: -118.407727},
+    map: map,
+  });
+
+var marker2 = new google.maps.Marker({
+    position: {lat: 33.665279, lng: -117.763177},
+    map: map2,
+  });
