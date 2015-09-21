41bf9cdab31017ae038123119da7ac635ebda859
dsun092 <dsun092@gmail.com>
2015-09-20 21:16:28 -0700

    re-size main cover picture

--- js/kazem.js
+++ js/kazem.js
@@ -11,22 +11,25 @@ $(window).bind('scroll', function () {
 var map = new google.maps.Map(document.getElementById('map1'), {
     center: {lat: 34.014440, lng: -118.407727},
     scrollwheel: false,
-    zoom: 13,
+    zoom: 11,
+    scaleControl: false,
+    draggable: false,
+    navigationControl: false,
+    scrollwheel: false,
+  });
+
+  var map2 = new google.maps.Map(document.getElementById('map2'), {
+    center: {lat: 33.665279, lng: -117.763177},
+    scrollwheel: false,
+    zoom: 11,
     scaleControl: false,
     draggable: false,
     navigationControl: false,
      scrollwheel: false,
   });
 
-  var map2 = new google.maps.Map(document.getElementById('map2'), {
-    center: {lat: 33.665279, lng: -117.763177},
-    scrollwheel: false,
-    zoom: 13,
-    scaleControl: false,
-    draggable: false,
-    navigationControl: false,
-     scrollwheel: false,
-  });
+google.maps.event.trigger(map, "resize");
+google.maps.event.trigger(map2, "resize");
  console.log("test");
 
 var marker1 = new google.maps.Marker({
