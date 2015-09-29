c3e52af6dbb1f24695e0adf7f6c5349ac1fabff1
David <david.sun@sadasystems.com>
2015-09-29 16:58:41 -0700

    wee

--- js/kazem.js
+++ js/kazem.js
@@ -7,37 +7,69 @@ $(window).bind('scroll', function () {
         $('.main_menu').removeClass('fixed');
     }
 });
+var map;
+var map2;
+var marker;
+var marker2;
 
-var map = new google.maps.Map(document.getElementById('map1'), {
-    center: {lat: 34.014440, lng: -118.407727},
-    scrollwheel: false,
-    zoom: 11,
-    scaleControl: false,
-    draggable: false,
-    navigationControl: false,
-    scrollwheel: false,
-  });
+$(window).load(function(){
+  setTimeout(function(){
+    map = new google.maps.Map(document.getElementById('map1'), {
+      center: {lat: 34.014440, lng: -118.407727},
+      scrollwheel: false,
+      zoom: 13,
+      scaleControl: false,
+      draggable: false,
+      navigationControl: false,
+      scrollwheel: false,
+    });
 
-  var map2 = new google.maps.Map(document.getElementById('map2'), {
-    center: {lat: 33.665279, lng: -117.763177},
-    scrollwheel: false,
-    zoom: 11,
-    scaleControl: false,
-    draggable: false,
-    navigationControl: false,
-     scrollwheel: false,
-  });
+    map2 = new google.maps.Map(document.getElementById('map2'), {
+      center: {lat: 33.665279, lng: -117.763177},
+      scrollwheel: false,
+      zoom: 13,
+      scaleControl: false,
+      draggable: false,
+      navigationControl: false,
+      scrollwheel: false,
+    });
 
-google.maps.event.trigger(map, "resize");
-google.maps.event.trigger(map2, "resize");
- console.log("test");
+    marker1 = new google.maps.Marker({
+      position: {lat: 34.014440, lng: -118.407727},
+      map: map,
+    });
 
-var marker1 = new google.maps.Marker({
-    position: {lat: 34.014440, lng: -118.407727},
-    map: map,
-  });
+    marker2 = new google.maps.Marker({
+      position: {lat: 33.665279, lng: -117.763177},
+      map: map2,
+    });
+    google.maps.event.trigger(map, 'resize');
+    google.maps.event.trigger(map2, 'resize');
+  }, 1000)
+})
 
-var marker2 = new google.maps.Marker({
-    position: {lat: 33.665279, lng: -117.763177},
-    map: map2,
-  });
+$('#english').click(function(){
+  var en = $('.en');
+  console.log(en);
+  $.each(en, function(item, value){
+    console.log(value);
+    $(value).css('display', 'inline');
+  })
+  var ar = $('.ar');
+  $.each(ar, function(item, value){
+    $(value).css('display', 'none');
+  })
+})
+
+$('#arabic').click(function(){
+  var ar = $('.ar');
+  $.each(ar, function(item, value){
+    $(value).css('display', 'inline');
+  })
+  var en = $('.en');
+  console.log(en);
+  $.each(en, function(item, value){
+    console.log(value);
+    $(value).css('display', 'none');
+  })
+})
