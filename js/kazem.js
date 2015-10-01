36dbcbc10cd755b099a4e5cb6dbfcd6adbebbd32
dsun092 <dsun092@gmail.com>
2015-09-30 23:59:44 -0700

    dope scroll

--- js/kazem.js
+++ js/kazem.js
@@ -71,3 +71,13 @@ $('#arabic').click(function(){
     $(value).css('display', 'none');
   })
 })
+
+$(document).ready(function() {
+  $('a[href^="#"]').click(function() {
+      var target = $(this.hash);
+      if (target.length == 0) target = $('a[name="' + this.hash.substr(1) + '"]');
+      if (target.length == 0) target = $('html');
+      $('html, body').animate({ scrollTop: target.offset().top-60 }, 1000);
+      return false;
+  });
+});
